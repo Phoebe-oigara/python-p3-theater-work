1443d6aebfb96feac2419f773482623bb17da556
Phoebe Oigara <phoebeoigara@gmail.com>
2023-06-13 12:24:22 +0300

    building my models

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,43 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+
+# Audition model
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String)
+    location = Column(String)
+    phone = Column(Integer)
+    hired = Column(Boolean, default=False)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    role = relationship("Role", back_populates="auditions")
+
+    def call_back(self):
+        self.hired = True
+
+# Role model
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String)
+
+    auditions = relationship("Audition", back_populates="role")
+
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        hired_audition = next((audition for audition in self.auditions if audition.hired), None)
+        return hired_audition or 'No actor has been hired for this role'
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[1] if len(hired_auditions) >= 2 else 'No actor has been hired for understudy for this role'
